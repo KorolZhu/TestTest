38cb8f71acc8eddeacb3a3d1219dcd5cbd6eb271
zhuzhi <zhuzhi2013@gmail.com>
2014-11-01 21:42:04 +0800

    Initial Commit

---
+++ WristBand/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WristBand
+//
+//  Created by zhuzhi on 14/11/1.
+//  Copyright (c) 2014年 WB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WristBand/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WristBand
+//
+//  Created by zhuzhi on 14/11/1.
+//  Copyright (c) 2014年 WB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
